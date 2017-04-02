8374d5bbf252f32733fdfb506a6bf7dd435ea197
MinxianLi <lmx423@gmail.com>
2017-04-02 14:25:27 -0500

    Add comment

--- insertData.php
+++ insertData.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "test";
 $dbname = "myDBPDO";
-
+// test
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
